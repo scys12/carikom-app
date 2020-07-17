cbfdd622fa1e89fe4f1c3e3a6a99605b9f396475
samuel s <samuel.18051@mhs.its.ac.id>
2020-07-18 00:28:05 +0700

    init react

---
+++ carikom/src/Item.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+class Item extends Component {
+    state = {
+        isLoading : true,
+        Items : []
+    };
+
+    async componentDidMount(){
+        const response = await fetch('/api/items');
+        const body = await response.json();
+        this.setState({
+            Items : body,
+            isLoading : false
+        });
+    }
+    render() { 
+        const {Items, isLoading} = this.state;
+        if (isLoading) {
+            return (<div>Loading...</div>);            
+        }
+        return (
+            <div>
+                <h2>
+                    Items
+                </h2>
+                {
+                    Items.map( item => 
+                        <div id={item.id}>
+                            {item.name}
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
+}
+ 
+export default Item;
